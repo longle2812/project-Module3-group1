d1be8d2ae47b296e3a3d7b99ef87ae11af0fd152
Long Le <lehailong2812@gmail.com>
2021-06-02 16:19:58 +0700

    test

--- src/main/java/Controller/BookServlet.java
+++ src/main/java/Controller/BookServlet.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class BookServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    int a = 1;
     }
 
     @Override
